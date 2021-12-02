a557740047f1f73c0cd22e4435ce493c257b8ca7
David <davhofer@ethz.ch>
2021-12-02 08:34:56 +0100

    edit

--- 1_short.py
+++ 1_short.py
@@ -1,10 +1,10 @@
+# solution for 1: last two variables in last line should be a
+# solution for 2: last two variables in last line should be b
 l=list
 a=l(map(int,open('1').readlines()))
 b=l(map(lambda x,y,z:x+y+z,a,a[1:],a[2:]))
 print(sum(map(lambda x,y:1 if x<y else 0,a,a[1:])))
 
-# solution for 1: last two variables in last line should be a
-# solution for 2: last two variables in last line should be b
 
 
 # for task 2, change l = inp to l = part2
